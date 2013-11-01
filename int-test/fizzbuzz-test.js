11a052bbabc04a9827c4c72c75a30954f5f79299
Your Name <you@example.com>
2013-11-01 22:25:18 +0100

    home-grown assertion

--- int-test/fizzbuzz-test.js
+++ int-test/fizzbuzz-test.js
@@ -1,8 +1,15 @@
+//run: phantomjs fizzbuzz-test.js
+
 var page = require('webpage').create();
 
 page.open('http://localhost:8888/fizzbuzz/15', function (status) {
-    console.log('status: ' + status);
-    console.log('document: ' + page.plainText);
+    testEquals('FizzBuzz', page.plainText);
     phantom.exit();
 });
+
+function testEquals(expected, actual) {
+  var success = expected === actual;
+  if (success) console.log('Pass');
+  else console.log("Failure: '" + expected + "' vs '" + actual + "'");
+}
 
